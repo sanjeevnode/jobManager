ac3e2a02ce656f877085055625ad97218d53269c
sanjeevnode <me.sanjeevks@gmail.com>
2024-11-24 12:22:44 +0530

    initial

---
+++ src/test/java/com/sanjeevnode/jobmanager/JobManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanjeevnode.jobmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
